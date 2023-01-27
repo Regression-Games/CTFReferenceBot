e0eb6218aa9fcafb944d10f7a1bdf89e28a3cadf
vontell <vontell@mit.edu>
2023-01-27 16:11:26 -0500

    New updates

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { RGBot } from "rg-bot";
 
+const BLUE_SCORE = [160, -385]
+
 /**
  * This strategy is the simplest example of how to get started with the rg-bot package.
  * The Bot will run around and gather Poppies until it has 100 in its inventory.
@@ -19,6 +21,11 @@ export function configureBot(bot: RGBot) {
             bot.chat("Did not find the flag")
         }
     }
+
+    bot.on("playerCollect", async (collector, collected) => {
+        bot.chat(JSON.stringify(collector))
+        bot.chat(JSON.stringify(collected))
+    })
 
     // Have the Bot begin our main loop when it spawns into the game
     bot.on('spawn', async () => {
