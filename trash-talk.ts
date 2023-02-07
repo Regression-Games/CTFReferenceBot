9ab5b035e7c8be8e9f78ff30e045b95d3250e88d
vontell <vontell@mit.edu>
2023-02-07 10:59:36 -0500

    New updates

--- trash-talk.ts
+++ trash-talk.ts
@@ -2,17 +2,21 @@
 import axios from "axios";
 export async function generateTrashTalk(phrase: string): Promise<string> {
 
-    const resp = await axios.post(
-        'https://vontell.steamship.run/rg-ctf-trash-talker/rg-ctf-trash-talker/generate',
-        {
-            body: JSON.stringify({phrase}),
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer E3D48BD0-36C2-423E-B419-FC0D5552B4E7'
+    try {
+        const resp = await axios.post(
+            'https://vontell.steamship.run/rg-ctf-trash-talker/rg-ctf-trash-talker/generate',
+            {
+                body: JSON.stringify({phrase}),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer E3D48BD0-36C2-423E-B419-FC0D5552B4E7'
+                }
             }
-        }
-    )
+        )
+        return JSON.stringify(resp.data);
+    } catch (e) {
+        return `Failed with: ${JSON.stringify(e)}`
+    }
 
-    return JSON.stringify(resp.data);
 
 }
