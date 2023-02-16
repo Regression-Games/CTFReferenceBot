f402324308240534ae07df504bbaaf653bdbf641
vontell <vontell@mit.edu>
2023-02-16 12:14:35 -0500

    Simplified bot

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# RG Bot Typescript Template
+# RG Capture the Flag Reference Bot (Typescript)
 
-This template is used to create a very simple bot for the Regression Games: Ultimate Collector challenge using typescript.  
-_Not sure what this is? Visit https://regression.gg for some programming fun!_
+This is a simple capture the flag example for Regression Games Capture the Flag game. The bot will repeatedly
+attempt to capture and score the flag.
+_Not sure what this is? Visit https://play.regression.gg for some programming fun!_
+
+## How to get started
+
+* Clone this repository to your GitHub
+* Create an account on https://play.regression.gg (add your GitHub and Minecraft account as well)
+* Create a new bot on https://play.regression.gg/bots
+  * Select "Create Bot"
+  * Select the "pick from my existing GitHub repository" options
+  * Select the repository you just cloned
+  * Add a bot name, and then click "Create Bot"
+* Click "Play" in the menu bar, and select the Capture the Flag game mode
+* Select "Solo", select your bot, and then queue up for a match!
 
 ## Minimum Requirements for Regression Games
 
@@ -20,7 +33,7 @@ Regression Games uses it as an entrypoint to your bot script, and passes a bot f
 Here is an example of the `configureBot` function with some basic logic that will make your bot parrot back
 anything it sees in chat from other players.
 
-```javascript
+```typescript
 export function configureBot(bot: RGBot) {
 
   // Every time a player says something in the game, 
--- index.ts
+++ index.ts
@@ -1,50 +1,19 @@
 import { RGBot } from "rg-bot";
-import Commander from "./commander";
-import {generateTrashTalk} from "./trash-talk";
 import {Entity} from "prismarine-entity";
 import {Item} from "prismarine-item";
 import RGCTFUtils from 'rg-ctf-utils';
 import {Vec3} from "vec3";
 
 /**
- * This strategy is the simplest example of how to get started with the rg-bot package.
- * The Bot will run around and gather Poppies until it has 100 in its inventory.
+ * This strategy is the simplest example of how to get started with the rg-bot and rg-ctf-utils package.
+ * The Bot will get the flag and then run back to base to score.
  */
 export function configureBot(bot: RGBot) {
 
     bot.setDebug(true);
     bot.allowDigWhilePathing(false);
     const ctfUtils = new RGCTFUtils(bot);
-    ctfUtils.debug = true;
-    const commander = new Commander(bot);
-
-    commander.register("start", async () => {
-        await ctfUtils.approachFlag();
-    })
-
-    bot.on('message', async (jsonMsg, position, sender, verified) => {
-        const enemyNames = ["DijkstrasPath"] // ctfUtils.getEnemyUsernames()
-        if (position == "system") {
-            const message = jsonMsg.extra[0]['text'];
-            if (message.includes("flag") && enemyNames.includes(message.split(" ")[0])) {
-                const trashTalk = await generateTrashTalk(message);
-                if (trashTalk) {
-                    bot.chat(trashTalk)
-                }
-            }
-        }
-    });
-
-    bot.on('chat', async (username: string, message: string) => {
-        const enemyNames = ["DijkstrasPath"] // ctfUtils.getEnemyUsernames()
-        if (username == bot.username()) return;
-        if (enemyNames.includes(username)) {
-            const trashTalk = await generateTrashTalk(message);
-            if (trashTalk) {
-                bot.chat(trashTalk)
-            }
-        }
-    });
+    ctfUtils.debug = false;
 
     ctfUtils.on('flagObtained', async (playerUsername: string) => {
         // If I was the one to obtain the flag, go and score!
@@ -54,14 +23,12 @@ export function configureBot(bot: RGBot) {
     });
 
     ctfUtils.on('flagScored', async (team: string) => {
-        // After scoring, collect items until the flag comes back
+        // The flag takes 10 seconds to respawn after scoring
         bot.chat("Flag scored, waiting until it respawns")
-        // await bot.findAndCollectItemsOnGround({maxDistance: 50});
-        // await bot.waitForMilliseconds(1000);
     })
 
     ctfUtils.on('flagAvailable', async (position: Vec3) => {
-        bot.chat("Flag is available, going to get it")
+        bot.chat("Flag is available (either dropped or spawned), going to get it")
         await ctfUtils.approachFlag();
     })
 
